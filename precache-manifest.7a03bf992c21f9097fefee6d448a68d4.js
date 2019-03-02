8d3fe7c17d4c0550bc04494845736774467f536b
Marvin Maciejewski <marvin.maciejewski@stud.hs-ruhrwest.de>
2019-03-02 08:59:20 +0100

    Updates

---
+++ precache-manifest.7a03bf992c21f9097fefee6d448a68d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "43fefb5a60e5fbff794c",
+    "url": "/my-react-gh-pages/static/js/runtime~main.43fefb5a.js"
+  },
+  {
+    "revision": "be65a0232e7a1120baba",
+    "url": "/my-react-gh-pages/static/js/main.be65a023.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/my-react-gh-pages/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "be65a0232e7a1120baba",
+    "url": "/my-react-gh-pages/static/css/main.380c5354.chunk.css"
+  },
+  {
+    "revision": "896e9f5b2c5c88a6f34baf1a17355d2a",
+    "url": "/my-react-gh-pages/index.html"
+  }
+];
