57fb49fa77fe8790fcbe211fa52efd058a553b1f
Marvin Maciejewski <marvin.maciejewski@stud.hs-ruhrwest.de>
2019-03-03 11:37:58 +0100

    Updates

---
+++ precache-manifest.0808517c56c1a951b0ecf2c84c2548dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "be6b3a478afad5c6829a",
+    "url": "/static/js/main.be6b3a47.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "be6b3a478afad5c6829a",
+    "url": "/static/css/main.4510ae9e.chunk.css"
+  },
+  {
+    "revision": "350c1f719870df822d4cb4d1b137a109",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-react-gh-page/precache-manifest.f2211c426c6200a40cb61cab44b2ccfd.js"
+  "/precache-manifest.0808517c56c1a951b0ecf2c84c2548dd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-react-gh-page/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
