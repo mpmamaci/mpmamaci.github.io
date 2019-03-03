200a4fcfea3c7be1aa88a3e398c4ef3312c53de9
Marvin Maciejewski <marvin.maciejewski@stud.hs-ruhrwest.de>
2019-03-03 12:02:37 +0100

    final fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            to be continued ?
+            to be continued !
           </p>
           <a
             className="App-link"
