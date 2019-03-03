eb32947055bf6ca4043fa51b18044b15fe9ef627
Marvin Maciejewski <marvin.maciejewski@stud.hs-ruhrwest.de>
2019-03-03 10:58:57 +0100

    fix: rm CNAME from wrong branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hay Laura :)
+            to be continued ..
           </p>
           <a
             className="App-link"
