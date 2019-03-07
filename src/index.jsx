56daa08751c81b5b62948128b6fa3d95022d68a2
Marvin Maciejewski <marvin.maciejewski@stud.hs-ruhrwest.de>
2019-03-07 19:21:40 +0100

    add promodoro

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './App/App';
+import * as serviceWorker from './service/serviceWorker';
 
-let document;
-
+// eslint-disable-next-line no-undef
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
