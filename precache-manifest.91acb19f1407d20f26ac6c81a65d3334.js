f23e4274b63d7b40bafe7cf7cd880273a3bedc2a
Marvin Maciejewski <marvin.maciejewski@stud.hs-ruhrwest.de>
2019-03-25 14:18:39 +0100

    Updates

--- precache-manifest.91acb19f1407d20f26ac6c81a65d3334.js
+++ precache-manifest.91acb19f1407d20f26ac6c81a65d3334.js
@@ -12,15 +12,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.42ac5946.js"
   },
   {
-    "revision": "7deece5064bd06e039f6",
-    "url": "/static/js/main.7deece50.chunk.js"
+    "revision": "cb06035a82842f7b03be",
+    "url": "/static/js/main.cb06035a.chunk.js"
   },
   {
     "revision": "be71b5eca68e3587816f",
     "url": "/static/js/2.be71b5ec.chunk.js"
   },
   {
-    "revision": "7deece5064bd06e039f6",
+    "revision": "cb06035a82842f7b03be",
     "url": "/static/css/main.a423009c.chunk.css"
   },
   {
@@ -28,7 +28,7 @@ self.__precacheManifest = [
     "url": "/static/css/2.b6329f58.chunk.css"
   },
   {
-    "revision": "680bd06414af54d60ceeb7c35aa3d6c6",
+    "revision": "a02d3e9b7987452d492200d6cf710a24",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.966af2876dcbac2dd8ae1b4869e78f54.js"
+  "/precache-manifest.91acb19f1407d20f26ac6c81a65d3334.js"
 );
 
 workbox.clientsClaim();
