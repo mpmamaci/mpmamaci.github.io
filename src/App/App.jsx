ec2b93198ada845f2a88f55d27cbe398d901582a
Marvin Maciejewski <marvin.maciejewski@stud.hs-ruhrwest.de>
2019-07-07 12:48:53 +0200

    add: clock to new path

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Pomodoro from '../Sites/Pomodoro/Pomodoro';
 import Home from '../Sites/Home/Home';
 import Cipher from '../Sites/MC2/Chiper';
+import Clock from '../Sites/Clock/Clock';
 import AppRouter from './AppRouter';
 import '../Sites/Pomodoro/Pomodoro.css';
 import 'bulma/css/bulma.css';
@@ -25,6 +26,10 @@ class App extends Component {
       {
         path: '/licenses',
         component: Licenses
+      },
+      {
+        path: '/clock',
+        component: Clock
       }
     ]
   };
