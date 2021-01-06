54fd4e5f887492b7a7515c325f718abc1c402e60
Marvin Maciejewski <maarvin1996@gmail.com>
2021-01-06 12:33:39 +0100

    convert into PWA

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
-import * as serviceWorker from './service/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
